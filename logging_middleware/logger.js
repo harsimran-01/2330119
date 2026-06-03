6fc083043e684b5bb65d2f452fc504f54dddbba4
Harsimran Kaur <140046087+harsimran-01@users.noreply.github.com>
2026-06-03 14:03:48 +0530

    Logger file api

--- logging_middleware/logger.js
+++ logging_middleware/logger.js
@@ -1,27 +1,27 @@
-const BASE_URL = "http://4.224.186.213/evaluation-service/logs";
+const API_URL =
+    "http://4.224.186.213/evaluation-service/logs";
 
 export const Log = async (
-  stack,
-  level,
-  packageName,
-  message
+    stack,
+    level,
+    packageName,
+    message
 ) => {
-  try {
-    const response = await fetch(BASE_URL, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        stack,
-        level,
-        package: packageName,
-        message,
-      }),
-    });
-
-    return await response.json();
-  } catch (error) {
-    console.error("Logger Error:", error);
-  }
+    try {
+        await fetch(API_URL, {
+            method: "POST",
+            headers: {
+                "Content-Type":
+                    "application/json",
+            },
+            body: JSON.stringify({
+                stack,
+                level,
+                package: packageName,
+                message,
+            }),
+        });
+    } catch (err) {
+        console.error(err);
+    }
 };
